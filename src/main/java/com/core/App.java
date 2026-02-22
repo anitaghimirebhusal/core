4848cc214a7031d0eac5350000c1e3e7bb0a904b
Anita Ghimire <anitaghimirebhusal@gmail.com>
2026-02-22 13:18:38 -0600

    second commit

--- src/main/java/com/core/App.java
+++ src/main/java/com/core/App.java
@@ -6,6 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class App {
     public static void main(String[] args) {
 
+
+        System.out.println("Hello World");
     }
 
     public static void springWithXML(){
